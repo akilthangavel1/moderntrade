314f4ab5e3efe92b09fada11d00c49bfc9145f8f
akilthangavel1 <akilthangavel@gmail.com>
2024-08-07 00:19:02 +0530

    Tem changed

--- tem.py
+++ tem.py
@@ -38,3 +38,37 @@ def get_intraday_data(symbol, interval='5m'):
 symbol = 'AAPL'
 data = get_intraday_data(symbol)
 print(data)
+
+
+[Unit]
+Description=gunicorn daemon
+Requires=gunicorn.socket
+After=network.target
+
+[Service]
+User=ubuntu
+Group=www-data
+WorkingDirectory=/home/ubuntu/moderntrade
+ExecStart=/home/ubuntu/moderntrade/venv/bin/gunicorn \
+          --access-logfile - \
+          --workers 3 \
+          --bind unix:/run/gunicorn.sock \
+          moderntrade.wsgi:application
+
+[Install]
+WantedBy=multi-user.target
+
+server {
+    listen 80;
+    server_name 51.20.191.107;
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location /static/ {
+        root /home/ubuntu/moderntrade;
+    }
+
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/run/gunicorn.sock;
+    }
+}

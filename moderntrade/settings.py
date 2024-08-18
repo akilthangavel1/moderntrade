071f77ebbdc3163e11aa31c0e45ec8d0bbd6523b
akilthangavel1 <akilthangavel@gmail.com>
2024-08-18 13:38:11 +0530

    models added

--- moderntrade/settings.py
+++ moderntrade/settings.py
@@ -73,16 +73,25 @@ WSGI_APPLICATION = 'moderntrade.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- scannerpro/models.py
+++ scannerpro/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class TickerBase(models.Model):
+    ticker_name = models.CharField(max_length=100, unique=True)
+    ticker_symbol = models.CharField(max_length=100, unique=True)
+    ticker_sector = models.CharField(max_length=20)
+    ticker_market_cap = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.ticker_symbol} ({self.ticker_sector}) - {self.ticker_market_cap}"

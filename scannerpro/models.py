f69ef243f61db2426b3fc8927bfc18da78a03a8f
akilthangavel1 <akilthangavel@gmail.com>
2024-08-19 11:24:43 +0530

    adding subsector

--- scannerpro/models.py
+++ scannerpro/models.py
@@ -4,6 +4,7 @@ class TickerBase(models.Model):
     ticker_name = models.CharField(max_length=100, unique=True)
     ticker_symbol = models.CharField(max_length=100, unique=True)
     ticker_sector = models.CharField(max_length=20)
+    ticker_sub_sector = models.CharField(max_length=20)
     ticker_market_cap = models.CharField(max_length=20)
 
     def __str__(self):

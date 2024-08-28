7317a28a38bcef8533dcf8b7ae737d1ee73e7ad5
akilthangavel1 <akilthangavel@gmail.com>
2024-08-28 22:23:17 +0530

    scanner updated

--- scannerpro/urls.py
+++ scannerpro/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
-    path('scanner/', views.scannerhome, name="scannerhome"),
+    # path('scanner/', views.scannerhome, name="scannerhome"),
     path('tickerlist/', views.ticker_list, name='ticker_list'),
     path('create/', views.ticker_create, name='ticker_create'),
     path('update/<int:pk>/', views.ticker_update, name='ticker_update'),
